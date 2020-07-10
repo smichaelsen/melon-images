e5aea1d473116893adfb1c2c60110ea2b43d57df
Sebastian Michaelsen <sebastian@michaelsen.io>
2020-07-10 10:43:20 +0200

    [BUGFIX] Don't register crop variants in CLI mode
    This breaks the typo3cms upgrade:all command

--- ext_tables.php
+++ ext_tables.php
@@ -2,6 +2,6 @@
 defined('TYPO3_MODE') || die();
 
 // in the FE this has to be done with a hook, after TypoScript has been initialized
-if (TYPO3_MODE === 'BE') {
+if (TYPO3_MODE === 'BE' && !(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_CLI)) {
     \Smichaelsen\MelonImages\TcaUtility::registerCropVariantsTcaFromTypoScript();
 }
