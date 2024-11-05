8e3a2bf992130fff60215c153eb18292d9d789d7
Georg Ringer <georg.ringer@gmail.com>
2024-11-05 07:39:34 +0100

    [TASK] Remove outdated code

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,5 @@
 <?php
 
-defined('TYPO3_MODE') || die();
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['melonImagesMigrateSchedulerTask'] = \Smichaelsen\MelonImages\Updates\MigrateSchedulerTask::class;
 
